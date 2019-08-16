1a99b3b82d93fa73f8726987b46e2cb0a05d80e0
Wen, Wen <wwen@ciena.com>
2019-08-16 16:40:20 -0400

    Initial trying and first commit

---
+++ LocalGameWeb/Pages/StartGame.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace LocalGameWeb.Pages
+{
+    public class StartGameModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
